86dcfdfd65ae6c3c74608af4042d087cdc3215b6
zhouxin <362790954@qq.com>
2019-07-16 11:03:02 +0800

    权限管理模块

---
+++ src/views/authority/roles.vue
@@ -0,0 +1,193 @@
+<template>
+  <div class="app-container">
+    <el-button type="primary" @click="handleAddRole">添加角色</el-button>
+
+    <el-table v-loading="loading" :data="rolesList" style="width: 100%;margin-top:30px;" border>
+      <el-table-column align="center" label="角色描述" prop="description" width="180" />
+      <el-table-column align="center" label="代码" prop="code" width="180" />
+      <el-table-column align="center" label="创建者" prop="createdBy" width="180" />
+      <el-table-column align="center" label="创建日期" prop="createdDate" width="180" />
+      <el-table-column align="center" label="最后修改着" prop="lastModifiedBy" width="180" />
+      <el-table-column align="center" label="最后修改时间" prop="lastModifiedDate" width="180" />
+      <el-table-column align="center" label="操作">
+        <template slot-scope="scope">
+          <el-button type="primary" size="small" @click="handleEdit(scope)">编辑</el-button>
+          <el-button type="danger" size="small" @click="handleDelete(scope)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <el-dialog :visible.sync="dialogVisible" :title="dialogType" :before-close="handleClose">
+      <el-form :ref="formRef" :model="role" label-width="80px" label-position="left">
+        <el-form-item label="角色名称" prop="description" :rules="[{ required: true }]">
+          <el-input v-model="role.description" placeholder="角色描述" />
+        </el-form-item>
+        <el-form-item label="角色代码" prop="code" :rules="[{ required: true }]">
+          <el-input v-model="role.code" placeholder="角色代码" />
+        </el-form-item>
+
+        <el-form-item label="菜单权限">
+          <el-tree
+            ref="tree"
+            :check-strictly="checkStrictly"
+            :data="routes"
+            :props="defaultProps"
+            show-checkbox
+            default-expand-all
+            node-key="id"
+            :default-checked-keys="defaultCheckedKeys"
+            class="permission-tree"
+          />
+        </el-form-item>
+      </el-form>
+      <div style="text-align:right;">
+        <el-button type="danger" @click="resetForm">取消</el-button>
+        <el-button type="primary" @click="confirmRole">保存</el-button>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import { getMenus, getRoles, saveRoles, deleteRoles, getMenusRoleMenus } from '@/api/authority'
+
+const defaultRole = {
+  code: '',
+  description: '',
+  menuIds: []
+}
+
+export default {
+  name: 'Roles',
+  data() {
+    return {
+      role: Object.assign({}, defaultRole),
+      loading: true,
+      routes: [],
+      rolesList: [],
+      defaultCheckedKeys: [],
+      dialogVisible: false,
+      dialogType: 'new',
+      formRef: 'formRef',
+      checkStrictly: false,
+      defaultProps: {
+        children: 'children',
+        label: 'title'
+      }
+    }
+  },
+  created() {
+    this.getRoutes()
+    this.getListMenus()
+  },
+  methods: {
+    // 获取角色列表
+    getRoutes() {
+      this.loading = true
+      getRoles()
+        .then(res => {
+          this.rolesList = res.data.list
+          this.loading = false
+        })
+    },
+    // 获取菜单
+    getListMenus() {
+      getMenus()
+        .then(res => {
+          this.routes = res.data
+        })
+    },
+    // 点击 添加角色 按钮
+    handleAddRole() {
+      this.$refs[this.formRef] && this.$refs[this.formRef].resetFields()
+      this.role = Object.assign({}, defaultRole)
+      if (this.$refs.tree) {
+        this.$refs.tree.setCheckedNodes([])
+        this.defaultCheckedKeys = []
+      }
+      this.dialogType = '添加角色'
+      this.dialogVisible = true
+    },
+    // 编辑角色
+    handleEdit(scope) {
+      this.$refs[this.formRef] && this.$refs[this.formRef].resetFields()
+      getMenusRoleMenus(scope.row.id)
+        .then(res => {
+          this.dialogType = '角色编辑'
+          this.role = Object.assign({}, scope.row)
+          this.dialogVisible = true
+          this.checkStrictly = true
+          this.$nextTick(() => {
+            this.checkStrictly = false
+            if (this.$refs.tree) {
+              this.$refs.tree.setCheckedNodes([])
+              this.defaultCheckedKeys = [...res.data]
+            }
+          })
+        })
+        .catch(err => {
+          this.$message.err(err)
+        })
+    },
+    // 删除角色
+    handleDelete({ $index, row }) {
+      this.$confirm('确认删除?', '警告', {
+        confirmButtonText: '确认',
+        cancelButtonText: '取消',
+        type: 'warning'
+      })
+        .then(() => {
+          deleteRoles(row.id)
+            .then(res => {
+              this.getRoutes()
+              this.$message.success(res.message)
+            })
+            .catch(err => {
+              this.$message.err(err)
+            })
+        })
+    },
+    // 保存角色
+    confirmRole() {
+      const checkedKeys = this.$refs.tree.getCheckedKeys()
+      const checkedHalfKeys = this.$refs.tree.getHalfCheckedKeys()
+      this.role.menuIds = [...checkedKeys, ...checkedHalfKeys]
+      this.$refs[this.formRef].validate((valid) => {
+        if (valid) {
+          saveRoles(this.role)
+            .then(res => {
+              this.$message.success(res.message)
+              this.getRoutes()
+              this.dialogVisible = false
+              this.defaultCheckedKeys = []
+            })
+            .catch(err => {
+              this.$message.error(err)
+            })
+        } else {
+          return false
+        }
+      })
+    },
+    resetForm() {
+      this.dialogVisible = false
+      this.$refs[this.formRef] && this.$refs[this.formRef].resetFields()
+    },
+    handleClose(done) {
+      done()
+      this.resetForm()
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .app-container {
+    .roles-table {
+      margin-top: 30px;
+    }
+    .permission-tree {
+      margin-bottom: 30px;
+    }
+  }
+</style>
---
+++ src/views/authority/users.vue
@@ -0,0 +1,263 @@
+<template>
+  <div class="app-container">
+    <el-button type="primary" @click="handleAdd">新增用户</el-button>
+
+    <el-table
+      v-loading="loading"
+      style="width: 100%; margin-top:20px;"
+      :data="tableData"
+      border
+      row-key="id"
+      fit
+      highlight-current-row
+    >
+      <el-table-column label="登陆名" prop="login" width="150" />
+      <el-table-column label="姓名" prop="name" width="80" />
+      <el-table-column label="邮箱" prop="email" min-width="220" />
+      <el-table-column label="手机号码" prop="phoneNumber" width="150" />
+      <el-table-column label="部门" prop="dept" width="80" />
+      <el-table-column label="职位" prop="position" width="150" />
+      <el-table-column label="操作" align="center" width="250">
+        <template slot-scope="scope">
+          <el-button type="primary" size="small" @click="handleEdit(scope)">编辑</el-button>
+          <el-button type="success" size="small" @click="handleAbout(scope)">详情</el-button>
+          <el-button type="danger" size="small" @click="handleDelete(scope)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <pagination
+      :total="total"
+      :page.sync="listQuery.page"
+      :limit.sync="listQuery.limit"
+      @pagination="getList"
+    />
+    <el-dialog :visible.sync="dialogVisible" :title="dialogType" :before-close="handleClose" top="5vh">
+      <el-form :ref="formRef" :model="formQ" label-width="110px" label-position="right" :disabled = "isHidden">
+        <el-form-item label="登陆名" prop="login" :rules="[{ required: true }]">
+          <el-input v-model="formQ.login" placeholder="输入登陆名" :disabled="isDisabled" />
+        </el-form-item>
+        <el-form-item label="姓名" prop="name" :rules="[{ required: true }]">
+          <el-input v-model="formQ.name" placeholder="输入姓名" />
+        </el-form-item>
+        <el-form-item v-if="['编辑用户','查看用户'].includes(dialogType)" label="性别" prop="gender">
+          <el-radio v-model="formQ.gender" label="1">男</el-radio>
+          <el-radio v-model="formQ.gender" label="2">女</el-radio>
+        </el-form-item>
+        <el-form-item label="邮箱" prop="email" :rules="[{ required: true }]">
+          <el-input v-model="formQ.email" placeholder="输入邮箱email" />
+        </el-form-item>
+        <el-form-item label="手机号码" prop="phoneNumber">
+          <el-input v-model="formQ.phoneNumber" placeholder="输入手机号码" />
+        </el-form-item>
+        <el-form-item label="部门" prop="dept">
+          <el-input v-model="formQ.dept" placeholder="输入部门" />
+        </el-form-item>
+        <el-form-item label="职位" prop="position">
+          <el-input v-model="formQ.position" placeholder="输入职位" />
+        </el-form-item>
+        <el-form-item label="SAP帐号" prop="sapAccount">
+          <el-input v-model="formQ.sapAccount" placeholder="输入SAP帐号" />
+        </el-form-item>
+        <el-form-item label="工厂" prop="factoryIds">
+          <el-select v-model="formQ.factoryIds" multiple placeholder="请选择" style="width: 100%">
+            <el-option v-for="item in factoryIds" :key="item.id + item.name" :label="item.code" :value="item.id">
+              <span style="float: left">{{ item.code + '  ----  ' + item.name }}</span>
+            </el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="角色" prop="roleIds">
+          <el-select v-model="formQ.roleIds" multiple placeholder="请选择" style="width: 100%">
+            <el-option v-for="item in roleIds" :key="item.id" :label="item.code" :value="item.id">
+              <span style="float: left">{{ item.code + '  ----  ' + item.description }}</span>
+            </el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item v-if="['编辑用户','查看用户'].includes(dialogType)" label="是否启用" prop="activated">
+          <el-radio v-model="formQ.activated" :label="true">启用</el-radio>
+          <el-radio v-model="formQ.activated" :label="false">关闭</el-radio>
+        </el-form-item>
+        <el-form-item v-if="isHidden" label="创建人" prop="createdBy">
+          <el-input v-model="formQ.createdBy" />
+        </el-form-item>
+        <el-form-item v-if="isHidden" label="创建时间" prop="createdDate">
+          <el-input v-model="formQ.createdDate" />
+        </el-form-item>
+        <el-form-item v-if="isHidden" label="最后操作人" prop="lastModifiedBy">
+          <el-input v-model="formQ.lastModifiedBy" />
+        </el-form-item>
+        <el-form-item v-if="isHidden" label="最后操作时间" prop="lastModifiedDate">
+          <el-input v-model="formQ.lastModifiedDate" />
+        </el-form-item>
+      </el-form>
+      <div style="text-align:right;">
+        <el-button type="danger" @click=" resetForm() ? '' : dialogVisible= false">取消</el-button>
+        <el-button type="primary" @click="submitForm" :disabled="isHidden">保存</el-button>
+      </div>
+    </el-dialog>
+
+  </div>
+</template>
+
+<script>
+
+import { getUsers, getOneUsers, getFactoriesAll, getRolesAll, saveUsers, deleteUsers } from '@/api/authority'
+import Pagination from '@/components/Pagination'
+const defaultForm = {
+  createdBy: '',
+  createdDate: '',
+  lastModifiedBy:	'',
+  lastModifiedDate: '',
+  id:	'',
+  login:	'',
+  name:	'',
+  email:	'',
+  activated:	1, // 是否激活 1激活
+  dept:	'',
+  position:	'',
+  sapAccount:	'',
+  phoneNumber:	'',
+  gender:	1, // 性别 1男 2女
+  factoryIds: [],
+  roleIds: []
+}
+
+export default {
+  name: 'Users',
+  components: { Pagination },
+  data() {
+    return {
+      formQ: Object.assign({}, defaultForm),
+      factoryIds: [],
+      roleIds: [],
+      tableData: [],
+      loading: true,
+      total: 1,
+      listQuery: {
+        page: 1,
+        size: 20, // 每页多少条
+        limit: 20, // 每页多少条
+        orderBy: 'id desc'// 升序:code 降序: code desc
+      },
+      formRef: 'formRef',
+      dialogType: '',
+      dialogVisible: false
+    }
+  },
+  computed: {
+    isDisabled() {
+      if (this.dialogType === '编辑用户') {
+        return true
+      } else {
+        return false
+      }
+    },
+    isHidden() {
+      if (this.dialogType === '查看用户') {
+        return true
+      } else {
+        return false
+      }
+    }
+  },
+  created() {
+    this.getList()
+    this.getFactories()
+    this.getRoles()
+  },
+  methods: {
+    initForm() {
+      this.formQ = Object.assign({}, defaultForm)
+    },
+    getList() {
+      this.loading = true
+      getUsers(this.listQuery)
+        .then(res => {
+          this.tableData = res.data.list
+          this.total = res.data.total
+          this.loading = false
+        })
+        .catch(err => {
+          this.$message.err(err)
+          this.loading = false
+        })
+    },
+    getFactories() {
+      getFactoriesAll()
+        .then(res => {
+          this.factoryIds = [...res.data]
+        })
+    },
+    getRoles() {
+      getRolesAll()
+        .then(res => {
+          this.roleIds = [...res.data]
+        })
+    },
+    handleAdd() {
+      this.dialogType = '添加用户'
+      this.dialogVisible = true
+    },
+    handleEdit({ row }) {
+      getOneUsers(row.id)
+        .then(res => {
+          this.formQ = { ...res.data }
+          this.$refs[this.formRef] && this.$refs[this.formRef].resetFields()
+          this.dialogType = '编辑用户'
+          this.dialogVisible = true
+        })
+    },
+    handleAbout({ row }) {
+      getOneUsers(row.id)
+        .then(res => {
+          this.formQ = { ...res.data }
+          this.$refs[this.formRef] && this.$refs[this.formRef].resetFields()
+          this.dialogType = '查看用户'
+          this.dialogVisible = true
+        })
+    },
+    handleDelete({ row }) {
+      this.$confirm('确认删除?', '警告', {
+        confirmButtonText: '确认',
+        cancelButtonText: '取消',
+        type: 'warning'
+      })
+        .then(() => {
+          deleteUsers(row.id)
+            .then(res => {
+              this.getList()
+              this.$message.success(res.message)
+            })
+            .catch(err => {
+              this.$message.err(err)
+            })
+        })
+    },
+    resetForm() {
+      this.initForm()
+      this.$refs[this.formRef] && this.$refs[this.formRef].resetFields()
+    },
+    submitForm() {
+      this.$refs[this.formRef].validate((valid) => {
+        if (valid) {
+          saveUsers(this.formQ)
+            .then(res => {
+              this.$message.success(res.message)
+              this.getList()
+              this.resetForm()
+              this.dialogVisible = false
+            })
+            .catch(err => {
+              this.$message.error(err)
+            })
+        } else {
+          return false
+        }
+      })
+    },
+    handleClose(done) {
+      done()
+      this.resetForm()
+    }
+  }
+}
+</script>
