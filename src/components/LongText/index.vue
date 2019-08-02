33b5490e1f89259dafc0d5a77027ad6654114aa3
zhouxin <362790954@qq.com>
2019-08-02 15:00:29 +0800

    支持长文本 组件

---
+++ src/components/LongText/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <span v-if="textLength <= maxLength">{{ text }}</span>
+    <el-tooltip v-else effect="dark" :content="text" placement="top">
+      <el-button type="text" style="margin: 0; padding: 0;">{{ text.toString().substring(0, maxLength) }}...</el-button>
+    </el-tooltip>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      default: ''
+    }
+  },
+  data() {
+    return {
+      maxLength: 10,
+      textLength: 0
+    }
+  },
+  created() {
+    this.textLength = this.text.length
+  },
+  methods: {
+  }
+}
+</script>
+
