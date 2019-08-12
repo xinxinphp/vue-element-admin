05d6a10c85361146359db98165071fd904ca7691
zhouxin <362790954@qq.com>
2019-08-12 09:46:03 +0800

    扩展LoneText插件

--- src/components/LongText/index.vue
+++ src/components/LongText/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <span v-if="textLength <= maxLength">{{ text }}</span>
+    <span v-if="text.length <= maxLength">{{ text }}</span>
     <el-tooltip v-else effect="dark" :content="text" placement="top">
       <el-button type="text" style="margin: 0; padding: 0; color: #606266">{{ text.toString().substring(0, maxLength) }}...</el-button>
     </el-tooltip>
@@ -13,16 +13,19 @@ export default {
     text: {
       type: String,
       default: ''
+    },
+    maxLength: {
+      type: Number,
+      default: 10
     }
   },
   data() {
     return {
-      maxLength: 10,
-      textLength: 0
     }
   },
   created() {
-    this.textLength = this.text.length
+  },
+  mounted() {
   },
   methods: {
   }
